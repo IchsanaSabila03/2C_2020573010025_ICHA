73d071e121fb4e3627ec66ebc3264e600902c6c1
ichsanasabila03 <ichsanasabila003@gmail.com>
2021-10-27 20:25:29 +0700

    praktikum6

--- praktikum_5_2020573010025_ichsana/5abukutamu.php
+++ praktikum_5_2020573010025_ichsana/5abukutamu.php
@@ -7,7 +7,7 @@
     <h1>Buku Tamu</h1>
     Komentar dan saran sangat kami butuhkan untuk meningkatkan kualitas situs kami.
     <hr>
-    <form action="proses_bukutamu.php" method="post">
+    <form action="5bproses_bukutamu.php" method="post">
         <pre>
             Nama anda : <input type="text" name="nama" size="25" maxlength="50">
             Email address : <input type="text" name="email" size="25" maxlength="50">
--- praktikum_5_2020573010025_ichsana/5bproses_bukutamu.php
+++ praktikum_5_2020573010025_ichsana/5bproses_bukutamu.php
@@ -1,24 +1,22 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <title>Buku Tamu</title>
-</head>
-<body>
+<html> 
+    <head>
+        <title>Buku Tamu</title>
+    </head>
+    <body>
     <?php
-    $nama = $_POST["nama"];
-    $email = $_POST["email"];
-    $komentar = $_POST["komentar"];
-    ?>
-    <h1>Data Buku Tamu</h1>
-    <hr>
-    Nama anda :
-    <?php echo $nama ?>
-    <br>
-    Email address :
-    <?php echo $email ?>
-    <br>
-    Komentar :
-    <textarea name="komentar" cols="40" rows="5"><?php echo $komentar ?></textarea>
-    <br>
-</body>
+        $nama = $_POST["nama"];
+        $email = $_POST["email"];
+        $komentar = $_POST["komentar"];
+        ?>
+        <h1>Data Buku Tamu</h1>
+        <hr>
+        Nama anda :
+        <?php echo $nama ?>
+        <br>
+        Email address :
+        <?php echo $email ?>
+        <br>
+        Komentar : <textarea name="komentar" cols="40" rows="5"><?php echo $komentar ?>
+        <br>
+    </body>
 </html>
