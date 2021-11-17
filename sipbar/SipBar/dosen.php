81ad544c9de7e7cf8013ca26ef215e76e8a2b04d
ichsanasabila03 <ichsanasabila003@gmail.com>
2021-11-17 12:06:42 +0700

    webb

--- sipbar/SipBar/dosen.php
+++ sipbar/SipBar/dosen.php
@@ -1,43 +1,41 @@
+<?php 
+require "proses/session.php";
+?>
 <!doctype html>
 <html lang="en">
   <head>
     <!-- Required meta tags -->
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+
     <!-- Bootstrap CSS -->
-    <link href="assets/dist/css/bootstrap.min.css" rel="stylesheet">
-  <title>HOME - Sistem Informasi Peminjaman Barang Jurusan Tik</title>
-</head>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+
+    <title>SIPBAR = Sistem Informasi Peminjaman Barang Jurusan TIK</title>
+  </head>
 
   <body>
-  <div class="container-fluid">
-    <!-- HEADER -->
-    <?php
-    require "header.php"
-    ?>
-    <!-- AKHIR HEADER -->
-  </div>
-
-  <div class="container-fluid">
-    <div class="row">
-      <div class="col-3">
-        <!-- SIDEBAR -->
+        <!--Header-->
         <?php
-        require "sidebar.php";
+        require "header.php";
         ?>
-      </div>
-     <!-- menutup  -->
-     
-     <div class="card col-8 ms-0 mt-6">
-        <div class="card-header">
-        SISTEM INFORMASI PEMINJAMAN BARANG JURUSAN TEKNOLOGI INFORMASI DAN KOMPUTER
-    (TIK) POLITEKNIK NEGERI LHOKSEUMAWE
-        </div>
-        </div>
-      <!-- AKHIR CONTENT -->
-    </div>
-  </div>
+        <!--Akhir Header-->
 
+        <div class="container-fluid">
+        <div class="row">
+          <div class="col-3">
+            <?php
+            require "sidebar.php";
+            ?>
+          </div>
+          <!--Akhir Sidebar-->
+          <!--Content-->
+          <div class="col-9">
+          <h2>DOSEN</h2>
+          </div>
+        <!--Akhir content-->
+        </div>
+      </div>
     <!-- Optional JavaScript; choose one of the two! -->
 
     <!-- Option 1: Bootstrap Bundle with Popper -->
--- sipbar/pinjam.php
+++
@@ -1,1 +0,0 @@
-<h2>Silahkan Isi Form Terlebih Dahulu</h2>
